18df890231a980f875dbefeb62ca8e9a4de44a8f
Truong Ha <truonghhq09@gmail.com>
2023-02-23 17:38:39 +0700

    adjust code

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,9 +1,37 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useEffect, useState} from "react";
 import {Button} from "antd";
+import {initializeApp} from "firebase/app";
+import {getFirestore, collection, getDocs } from "firebase/firestore";
 import {connect} from "react-redux";
 import "./style.scss";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyAzIMEbpaLPABHmfbAtKODOKcji8VXdLlM",
+    authDomain: "zens-test.firebaseapp.com",
+    databaseURL: "https://zens-test-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "zens-test",
+    storageBucket: "zens-test.appspot.com",
+    messagingSenderId: "504567204300",
+    appId: "1:504567204300:web:e7e926437649642a29f569",
+    measurementId: "G-JKYCBMS238"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 const HomePage = (props) => {
+    const [listStory, setListStory] = useState([]);
+
+    useEffect(() => {
+        getDocs(collection(db, "stories")).then(querySnapshot => {
+            querySnapshot.forEach((doc) => {
+                setListStory(doc.data());
+            });
+            return null;
+        });
+    }, []);
+
     return (
         <Fragment>
             <div className="block-home-page">
@@ -13,10 +41,14 @@ const HomePage = (props) => {
                 </div>
                 <div className="joke">
                     <div className="wrap-text">
-                        <p>A child asked his father, "How were people born?" So his father said, "Adam and Eve made babies, then their
-                            babies became adults and made babies, and so on." The child then went to his mother, asked her the same
-                            question and she told him, "We were monkeys then we evolved to become like we are now." The child ran back
-                            to his father and said, "You lied to me!" His father replied, "No, your mom was talking about her side of the
+                        <p>A child asked his father, "How were people born?" So his father said, "Adam and Eve made
+                            babies, then their
+                            babies became adults and made babies, and so on." The child then went to his mother, asked
+                            her the same
+                            question and she told him, "We were monkeys then we evolved to become like we are now." The
+                            child ran back
+                            to his father and said, "You lied to me!" His father replied, "No, your mom was talking
+                            about her side of the
                             family."</p>
                     </div>
                     <div className="wrap-action">
