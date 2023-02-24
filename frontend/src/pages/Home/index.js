0482c7d09180264db70cecc619dffa9566abfe59
Truong Ha <truonghhq09@gmail.com>
2023-02-24 09:56:08 +0700

    adjust code

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,8 +1,8 @@
 import React, {Fragment, useEffect, useState, useCallback, useRef} from "react";
-import {Button} from "antd";
+import {Button, Modal} from "antd";
 import {initializeApp} from "firebase/app";
 import Loading from "../../components/Loading";
-import {getFirestore, query, collection, getDocs} from "firebase/firestore";
+import {getFirestore, query, collection, doc, getDocs, updateDoc} from "firebase/firestore";
 import {connect} from "react-redux";
 import "./style.scss";
 
@@ -26,6 +26,8 @@ const HomePage = (props) => {
     const [loading, setLoading] = useState(false);
     const [listStory, setListStory] = useState([]);
 
+    const [modal, contextHolder] = Modal.useModal();
+
     const hdlListStory = useCallback((trigger = false) => {
         if (trigger) {
             isRendered.current = false;
@@ -39,14 +41,14 @@ const HomePage = (props) => {
                 queryFt.forEach((docFt) => {
                     let outDt = docFt.data();
                     if (!outDt.like.includes("jim_hls") && !outDt.dislike.includes("jim_hls")) {
-                        tempStoryNew.push(docFt.data());
+                        let tempDocFt = {key: docFt.id, ...docFt.data()};
+                        tempStoryNew.push(tempDocFt);
                     }
                 });
                 setListStory(tempStoryNew);
             }
             return null;
         }).catch(error => {
-            console.log(error);
             if (!isRendered.current) {
                 setLoading(false);
                 console.log("HomePage - hdlListStory");
@@ -66,16 +68,79 @@ const HomePage = (props) => {
         };
     }, [hdlListStory]);
 
+    const onPressFunny = async (data) => {
+        setLoading(true);
+        let tempLike = data.like;
+        tempLike.push("jim_hls");
+        const updateStory = doc(db, "stories", data.key);
+        await updateDoc(updateStory, {
+            "like": tempLike
+        }).then(() => {
+            setLoading(false);
+            hdlListStory();
+        }).catch(error => {
+            const text = error.message ? error.message : "Lỗi không rõ từ máy chủ";
+            modal.warning({
+                title: "Thông báo",
+                okText: "Đóng",
+                centered: true,
+                content: text
+            });
+            setLoading(false);
+        });
+    };
+
+    const onPressNotFunny = async (data) => {
+        setLoading(true);
+        let tempLike = data.like;
+        tempLike.push("jim_hls");
+        const updateStory = doc(db, "stories", data.key);
+        await updateDoc(updateStory, {
+            "dislike": tempLike
+        }).then(() => {
+            setLoading(false);
+            hdlListStory();
+        }).catch(error => {
+            const text = error.message ? error.message : "Lỗi không rõ từ máy chủ";
+            modal.warning({
+                title: "Thông báo",
+                okText: "Đóng",
+                centered: true,
+                content: text
+            });
+            setLoading(false);
+        });
+    };
+
     const yieldStory = () => {
         if (listStory.length > 0) {
             return <>
-                <p>
-                    {listStory[0].content}
-                </p>
+                <div className="wrap-text">
+                    <p>
+                        {listStory[0].content}
+                    </p>
+                </div>
+                <div className="wrap-action">
+                    <div className="box-button-event">
+                        <Button className="btn-like ant-btn-lg"
+                                onClick={onPressFunny.bind(this, listStory[0])}
+                                htmlType="button">
+                            <span>This is Funny!</span>
+                        </Button>
+                        <Button className="btn-unlike ant-btn-lg"
+                                onClick={onPressNotFunny.bind(this, listStory[0])}
+                                htmlType="button">
+                            <span>This is not funny!</span>
+                        </Button>
+                    </div>
+                </div>
             </>
-        } else {
+        }
+        if (listStory.length === 0 && !loading) {
             return <>
-                <p>That's all the jokes for today! Come back another day!</p>
+                <div className="wrap-text">
+                    <p>That's all the jokes for today! Come back another day!</p>
+                </div>
             </>;
         }
     };
@@ -88,26 +153,11 @@ const HomePage = (props) => {
                     <p>If you joke wrong way, your teeth have to pay. (Serious)</p>
                 </div>
                 <div className="joke">
-                    <div className="wrap-text">
-                        {yieldStory()}
-                    </div>
-                    <div className="wrap-action">
-                        <div className="box-button-event">
-                            <Button className="btn-like ant-btn-lg"
-                                    disabled={!listStory.length}
-                                    htmlType="button">
-                                <span>This is Funny!</span>
-                            </Button>
-                            <Button className="btn-unlike ant-btn-lg"
-                                    disabled={!listStory.length}
-                                    htmlType="button">
-                                <span>This is not funny!</span>
-                            </Button>
-                        </div>
-                    </div>
+                    {yieldStory()}
                 </div>
             </div>
             <Loading open={loading}/>
+            {contextHolder}
         </Fragment>
     );
 };
